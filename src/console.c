bd790209bc50079fd2c7f40b59c7475e18b42680
yovankas <117987991+yovankas@users.noreply.github.com>
2023-11-17 14:32:51 +0700

    nunggu function save + start (tambahin boolean startcheck)

---
+++ src/console.c
@@ -0,0 +1,58 @@
+#include "console.h"
+
+void quit()
+{
+    printf("Apakah kamu ingin menyimpan data sesi sekarang? ");
+    char confirm;
+    scanf("%c", &confirm);
+    if (confirm == 'Y')
+    {
+        printf("manggil save disini\n");
+        //save(); // BELUM ADA FUNCTIONNYA
+
+    }
+    printf("Kamu keluar dari WayangWave.\n");
+    printf("Dadah ^_^/\n");
+}
+
+void help(boolean startcheck)
+{
+    if(!startcheck)
+    {
+        printf("=========================================================[ Menu Help WayangWave ]=========================================================\n");
+        printf("1. START -> Untuk masuk sesi baru\n");
+        printf("2. LOAD -> Untuk memulai sesi berdasarkan file konfigurasi\n");
+    }
+    else 
+    {
+        printf("=========================================================[ Menu Help WayangWave ]=========================================================\n");
+        printf("1. LIST DEFAULT -> Untuk menampilkan list penyanyi, list album dari seorang penyanyi, serta list lagu dari suatu album\n");
+        printf("2. LIST PLAYLIST -> Untuk menampilkan playlist yang ada pada pengguna\n");
+        printf("3. PLAY SONG -> Untuk memainkan lagu berdasarkan nama penyanyi, nama album, dan ID lagu\n");
+        printf("4. PLAY PLAYLIST -> Untuk memainkan lagu berdasarkan ID playlist\n");
+        printf("5. QUEUE SONG -> Untuk menambahkan lagu ke dalam queue\n");
+        printf("6. QUEUE PLAYLIST -> Untuk menambahkan lagu yang ada dalam playlist ke dalam queue\n");
+        printf("6. QUEUE SWAP <x> <y> -> Untuk menukar lagu pada urutan ke (x) dan juga urutan ke (y)\n");
+        printf("7. QUEUE REMOVE <id> -> Untuk menghapus lagu dari queue dengan inputan urutan (id) lagu yang ingin dihapus dari queue\n");
+        printf("8. QUEUE CLEAR -> Untuk mengosongkan queue\n");
+        printf("9. SONG NEXT -> Untuk memutar lagu selanjutnya dalam queue jika ada\n");
+        printf("10. SONG PREVIOUS -> Untuk memutar lagu yang sebelumnya diputar jika ada\n");
+        printf("11. PLAYLIST CREATE -> Untuk membuat playlist baru dan ditambahkan pada daftar playlist pengguna\n");
+        printf("12. PLAYLIST ADD -> Untuk menambahkan lagu atau album ke dalam playlist yang sudah dimiliki pengguna dalam daftar playlist\n");
+        printf("13. PLAYLIST SWAP <id> <x> <y> -> Untuk menukar lagu pada urutan ke (x) dan juga urutan ke (y) di playlist dengan index (id)\n");
+        printf("14. PLAYLIST REMOVE <id> <n> -> Untuk menghapus lagu dengan urutan (n) pada playlist dengan index (id)\n");
+        printf("15. PLAYLIST DELETE -> Untuk melakukan penghapusan suatu existing playlist dalam daftar playlist\n");
+        printf("16. STATUS -> Untuk menampilkan lagu yang sedang diputar, dari playlist mana lagu tersebut, serta queue song (jika ada)\n");
+        printf("17. SAVE <filename> -> Untuk menyimpan state WayangWave terbaru ke dalam suatu file bernama (filename) dengan tipe txt\n");
+        printf("18. QUIT -> Untuk keluar dari aplikasi WayangWave\n");
+    }
+}
+
+int main ()
+{
+    boolean start = false;
+    help(start);
+    start = true;
+    help(start);
+    return 0;
+}
---
+++ src/console.h
@@ -0,0 +1,30 @@
+#ifndef __CONSOLE_H__
+#define __CONSOLE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./boolean.h"
+#include "./ADT/array/array.h"
+#include "./ADT/word/word.h"
+#include "./ADT/mesin/mesinkata.h"
+#include "./ADT/mesin/mesinkarakter.h"
+#include "./ADT/set/set.h"
+#include "./ADT/album/album.h"
+#include "./ADT/penyanyi/penyanyi.h"
+#include "./ADT/string/string.h"
+
+void quit();
+/*
+ * Konstruktor
+ * I.S. Program berjalan
+ * F.S. Keluar dari program dengan atau tanpa menyimpan data di sesi tersebut
+ */
+
+void help(boolean startcheck);
+/**
+ * Konstruktor
+ * I.S. Program berjalan
+ * F.S. Menampilkan daftar command yang mungkin untuk dieksekusi dengan deskripsinya
+ */ 
+#endif
