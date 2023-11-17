32ae618ebcaaa393ab5db7d5c9ee36874c66c7fc
Christoper Daniel <christoper.daniel04@gmail.com>
2023-11-17 19:49:01 +0700

    ini adt untuk playlist lagu

--- src/ADT/playlist/playlist.h
+++ src/ADT/playlist/playlist.h
@@ -4,35 +4,76 @@
 #include <stdio.h>
 #include "../../boolean.h"
 #include "../../adt/mesin/mesinkata.h"
-#include "../../adt/album/album.h"
 #include "../../adt/array/array.h"
-#include "../../ADT/playlist/listlinier.h"
+#include "../../ADT/set/set.h"
+#include "../../ADT/map/map.h"
 
-void CREATEPlaylist();
-/* I.S. Sembarang*/
-/* F.S. Membuat playlist baru kosong*/
+// #define false 0
+// #define true 1
+#define NilPlaylist 0
+#define MaxElPlaylist 100
 
-void ADDSongPlaylist();
-/* I.S. Playlist mungkin kosong, Playlist tidak penuh*/
-/* F.S. Melakukan alokasi sebuah song dan ditambahkan sebagai elemen list di akhir*/
+typedef int keytype;
+typedef Set valuetype;
+typedef int addressPlaylist;
 
-void ADDAlbumPlaylist();
-/* I.S. Playlist mungkin kosong, Playlist tidak penuh*/
-/* F.S. Melakukan alokasi sebuah album dan ditambahkan sebagai elemen list di akhir*/
+typedef struct{
+    keytype Key;
+    valuetype Value;
+} infotypePlaylist;
 
-void SWAPSongPlaylist();
-/* I.S. Playlist tidak boleh kosong*/
-/* Menukar posisi lagu urutan x dan urutan y di playlist urutan id*/
-/* F.S. Lagu di urutan x dan y di playlist id sudah ditukar*/
+typedef struct{
+    infotypePlaylist Elements[MaxElPlaylist];
+    addressPlaylist Count;
+} Playlist;
 
-void REMOVESongPlaylist();
-/* I.S. Playlist tidak boleh kosong*/
-/* Menghapus lagu dengan urutan n pada playlist dengan index id.*/
-/* F.S. elemen lagu urutan n pada playlist id sudah dihapus dan di-dealokasi*/
+/* Definisi Playlist PL kosong : PL.Count = Nil */
+/* PL.Count = jumlah element Playlist */
+/* PL.Elements = tempat penyimpanan element Playlist */
 
-void DELETEPlaylist();
-/* I.S. Daftar Playlist tidak boleh kosong*/
-/* Menghapus playlist pada daftar playlist dengan index id.*/
-/* F.S. Playlist yang ada di daftar playlist pengguna dihapus dan di-dealokasi*/
+/* ********* Prototype ********* */
+
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty_Playlist(Playlist *PL);
+/* I.S. Sembarang */
+/* F.S. Membuat sebuah Playlist PL kosong berkapasitas MaxEl */
+/* Ciri Playlist kosong : count bernilai Nil */
+
+/* ********* Predikat Untuk test keadaan KOLEKSI ********* */
+boolean IsEmpty_Playlist(Playlist PL);
+/* Mengirim true jika Playlist PL kosong*/
+/* Ciri Playlist kosong : count bernilai Nil */
+
+boolean IsFull_Playlist(Playlist PL);
+/* Mengirim true jika Playlist PL penuh */
+/* Ciri Playlist penuh : count bernilai MaxEl */
+
+/* ********** Operator Dasar Playlist ********* */
+valuetype Value_Playlist(Playlist PL, keytype k);
+/* Mengembalikan nilai value dengan key k dari PL */
+/* Jika tidak ada key k pada PL, akan mengembalikan Undefined */
+
+void Insert_Playlist(Playlist *PL, keytype k, valuetype v);
+/* Menambahkan Elmt sebagai elemen Playlist PL. */
+/* I.S. PL mungkin kosong, PL tidak penuh
+        PL mungkin sudah beranggotakan v dengan key k */
+/* F.S. v menjadi anggota dari PL dengan key k. Jika k sudah ada, operasi tidak dilakukan */
+
+void Delete_Playlist(Playlist *PL, keytype k);
+/* Menghapus Elmt dari Playlist PL. */
+/* I.S. PL tidak kosong
+        element dengan key k mungkin anggota / bukan anggota dari PL */
+/* F.S. element dengan key k bukan anggota dari PL */
+
+boolean IsMember_Playlist(Playlist PL, keytype k);
+/* Mengembalikan true jika k adalah member dari PL */
+
+void Copy_Playlist(Playlist *PL1, Playlist PL2);
+/* Melakukan copy Elmt dari Playlist PL2 ke PL1. */
+/* I.S. PL1 dan PL2 terdefinisi
+/* F.S. elemen dari PL2 dicopy ke PL1 */
+
+void PrintPlaylistLagu(Playlist PL, keytype k);\
+/* Melakukan print playlist*/
 
 #endif
